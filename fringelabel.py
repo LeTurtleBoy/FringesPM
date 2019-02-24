ef0dc622ab45e60bdb54f81beffdf1192acfbcfd
LeTurtleBoy <christiand.moreno@udea.edu.co>
2019-02-24 01:42:32 -0500

    Mejoras

--- fringelabel.py
+++ fringelabel.py
@@ -254,16 +254,18 @@ class FringeAnalysis:
         else:
             print("First load any valid image")
 
-    def process_inter_distance(self, dir_image):
+    def load_inter_distance(self, dir_image):
         name_aux = dir_image.split('.')
         print("Loading {} vertical and horizontal...".format(dir_image[0]))
         
         if os.path.isfile(name_aux[0] + "_v." + name_aux[1]):
-            self.image_v = self.bin_array(Image.open(name_aux[0] + "_v." + name_aux[1]), 1)
+            self.image_v = self.bin_array(np.array(Image.open(name_aux[0] + "_v." + name_aux[1])),background=1)
             self.image_v = measure.label(self.image_v)
+            self.process_id("Vertical")
         if os.path.isfile(name_aux[0] + "_h." + name_aux[1]):
-            self.image_h = self.bin_array(Image.open(name_aux[0] + "_h." + name_aux[1]), 1)
+            self.image_h = self.bin_array(np.array(Image.open(name_aux[0] + "_h." + name_aux[1])),background=1)
             self.image_h = measure.label(self.image_h)
+            self.process_id("Horizontal")
         if (self.image_v == []):
             print("no hay imagenes verticales")
         if( self.image_h == []):
@@ -271,5 +273,77 @@ class FringeAnalysis:
         if ( (self.image_v == []) &  (self.image_h==[] )):
             print("No hay imagenes procesadas Horizontales o Verticales")
             return 0
+        
+    def process_id(self, mode):
+        if mode == "Horizontal":
+            I, J = np.nonzero(self.image_h)
+            F = [[0, 0, 0]]*len(I)
+            for x in range(len(I)):
+                F[x]= [self.image_h[I[x]][J[x]],I[x],J[x]]
+            df =  pd.DataFrame(F,columns = ['Fringe_label','x','y'])
+            print(df.head())
+            groups = df.groupby('Fringe_label')
+            data_size = len(groups.groups)
+            Vector = [0]*data_size
+            u = 0
+            for ref, df in groups:
+                Vector[u] = df.values
+                u+=1
+
+            pure_data = []
+            n = 0
+            for i in range(len(Vector)):
+                name = "process fringe " + str(i+1) + " of "+ str(len(Vector)-1) + " possible couples"
+                total_it = len(Vector) - i
+                pb = ProgressBar(total = total_it, prefix = name,suffix='Ok', decimals=3, length=50, fill='X',zfill='-')
+                for j in range(i+1, len(Vector)):
+                    pb.print_progress_bar(j-i)
+                    l = 0
+                    base = Vector[i]
+                    aux = Vector[j]
+                    aux_data = []
+                    for row in base:
+                        for row_ in aux:
+                            a = row[1]-row_[1]
+                            b = row[2]-row_[2]
+                            if a == 0: angle = np.pi/2 * np.sign(b)
+                            elif b == 0: angle = np.pi * int(x<0)
+                            else: angle = np.arctan(b/a) 
+                            angle = angle*(180.0 / np.pi)
+                            distance = np.sqrt((a)**2 + (b)**2)
+                            if ((distance > 60) | (-45 > angle) | (angle < 45)):
+                                pass
+                            else:
+                                if aux_data == []:
+                                    aux_data = [row,np.append(row_,[distance, angle])]
+                                else:
+                                    aux_data.append([row,np.append(row_,[distance, angle])])
+                                l-=-1
+                    if (aux_data != []):
++
+                        if  pure_data != []:
+                            pure_data.append(aux_data)
+                        else:
+                            pure_data = aux_data
+                        n-=-1
+                pb.print_progress_bar(total_it)
+                break
+            Datos = []
+            for i in pure_data:
+                if i != 0: 
+                    Datos.append(i)
+
+            print("Done")
+            a = pd.DataFrame(Datos)
+            a.to_excel("indis.xlsx", index=False)
+            print("final i guess?")
+
+                     
+
+                    
+'''fringe = FringeAnalysis()
+fringe.load_inter_distance("590kx_WF_RD30_07_skeleton.tif")#"590kx_WF_RD30_07_skeleton_final.tif"
+
+'''
         
         
--- main.py
+++ main.py
@@ -3,5 +3,4 @@ import fringelabel as fringe
 fa = fringe.FringeAnalysis()
 #fa.load_image("590kx_WF_RD30_07_skeleton.tif")
 #fa.process_lc()
-fa.process_inter_distance("590kx_WF_RD30_07_skeleton.tif")#"590kx_WF_RD30_07_skeleton_final.tif"
-
+fa.load_inter_distance("590kx_WF_RD30_07_skeleton.tif")#"590kx_WF_RD30_07_skeleton_final.tif"
